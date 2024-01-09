519a01bcc58a83997628ac6818126a1cecce429d
zyronon <zyronon@163.com>
2024-01-09 10:22:45 +0800

    save

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     monkey({
       entry: 'src/main.ts',
       userscript: {
-        version: '7.8.5',
+        version: '7.8.7',
         name: 'V2EX Next',
         icon: 'https://www.google.com/s2/favicons?sz=64&domain=v2ex.com',
         namespace: 'http://tampermonkey.net/',
