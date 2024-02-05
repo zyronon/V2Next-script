364dbb79d287f2e8949ed0cb0cc0728cf5b7ef66
zyronon <zyronon@163.com>
2024-02-05 14:44:49 +0800

    修改modal层级

--- v2next/pc/vite.config.ts
+++ v2next/pc/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       clientAlias: 'gmApi',
       entry: 'src/main.ts',
       userscript: {
-        version: '8.1.0',
+        version: '8.1.1',
         name: 'V2Next',
         icon: 'https://v2next.netlify.app/favicon.ico',
         namespace: 'http://tampermonkey.net/',
